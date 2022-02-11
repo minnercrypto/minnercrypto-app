be432adbdd83c418e5ec576bf1d8776156fc8903
minnercrypto <87305316+minnercrypto@users.noreply.github.com>
2022-02-11 08:07:23 -0800

    asdasda
    sdasdasd

--- dash/assets/js/config/firebase.config.js
+++ dash/assets/js/config/firebase.config.js
@@ -1,11 +1,11 @@
 var firebaseConfig = {
-  apiKey: "AIzaSyCCo8t7JAXcqhe3CNK6dqsQWVU3QuZff6U",
-  authDomain: "crypto-money-1d5f4.firebaseapp.com",
-  projectId: "crypto-money-1d5f4",
-  storageBucket: "crypto-money-1d5f4.appspot.com",
-  messagingSenderId: "858203034619",
-  appId: "1:858203034619:web:17a5350babea8acd53315e",
-  measurementId: "G-NFH9K2S5P8"
+  apiKey: "AIzaSyA7TglVggnyZ5a_rVEMPiuqnK3JesTGjic",
+  authDomain: "currency-market-miner.firebaseapp.com",
+  projectId: "currency-market-miner",
+  storageBucket: "currency-market-miner.appspot.com",
+  messagingSenderId: "170574948592",
+  appId: "1:170574948592:web:69e368c2c82646d3313a35",
+  measurementId: "G-T6EVM8BCWE"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
